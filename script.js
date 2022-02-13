c828636f064609e063f22b7289d24630b4e7d2a5
Tiago Suhet <41129969+tiagosuhet@users.noreply.github.com>
2022-02-13 17:36:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+jokeBtn.addEventListener('click', generateJoke);
+generateJoke();
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  const res = await fetch('https://icanhazdadjoke.com', config);
+  const data = await res.json();
+  console.log(data.joke);
+  jokeEl.innerHTML = data.joke;
+}
